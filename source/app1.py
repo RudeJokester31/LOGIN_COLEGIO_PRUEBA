f6807d399c04e6ee16badc25d733103d15ebba1b
ejhostin0431 <jhostinepalza0411@gmail.com>
2023-02-06 22:33:01 -0500

    Version de prueba api_login colegio

---
+++ source/app1.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL
+from flask_login import LoginManager, login_user, logout_user, login_required
+from flask_wtf.csrf import CSRFProtect
+from config1 import config
+
+from models.ModelUser import ModelUser
+
+from models.entities.User import User
+
+app = Flask(__name__)
+
+csrf=CSRFProtect()
+db=MySQL(app)
+login_manager_app = LoginManager(app)
+
+
+@login_manager_app.user_loader
+def load_user(id):
+    return ModelUser.get_by_id(db,id)
+
+@app.route("/")
+def index():
+    return redirect(url_for("login"))
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method=="POST":
+        # print(request.form ["Username"])
+        # print(request.form ["Password"])
+        user=User(0,request.form['username'],request.form['password'])
+        logged_user=ModelUser.login(db,user)
+        if logged_user != None:
+            if logged_user.password:
+                login_user(logged_user)
+                return redirect(url_for("home"))
+            else:
+                flash("Credenciales de acceso invalidas")
+            return render_template('auth/login.html')
+        else:
+            flash("El usuario no se encuentra registrado en nuestra base de datos")
+        return render_template('auth/login.html')
+    else:
+        return render_template('auth/login.html')
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
+
+
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/protected")
+@login_required
+def protected():
+    return "<h1>Esta es una vista protegida, solo para usuarios autenticados</h1>"
+
+def status_401(error):
+    return redirect(url_for("login"))
+
+def status_404(error):
+    return "<h1>Página no encontrada</h1>"
+
+
+if __name__ == '__main__':
+    app.config.from_object(config["development"])
+    csrf.init_app(app)
+    app.register_error_handler(401,status_401)
+    app.register_error_handler(404,status_404)
+    app.run()
